39c2853268259832a744ce9e55b07b524069552d
olismajid <43783286+olismajid@users.noreply.github.com>
2018-10-31 13:39:41 +0700

    frist commit

---
+++ soal 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{int N,X,T,Batas;
+N = 00;
+Batas = 00 + 100;
+X = 20;
+T = 00;
+while( T <= Batas)
+     { T = T + X;
+       X = X + 10;
+     }
+ cout << T;
+
+}
